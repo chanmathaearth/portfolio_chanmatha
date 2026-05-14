ed6fdcae66ff7e26804d4d3e2a8899aeb6dee79a
chanmathaearth <chanmathaearth@gmail.com>
2026-05-15 04:27:25 +0700

    feat:hero changed profile picture on hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -71,7 +71,7 @@ export const Hero = () => {
               <img
                 src={profilePic}
                 alt="Profile"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-contain scale-120"
               />
             </div>
           </motion.div>
